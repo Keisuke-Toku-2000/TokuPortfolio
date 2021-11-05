2291bd4628a2b2ad5fd974b16f6f727a377c6b36
Keisuke-Toku-2000 <keisuke20000923@gmail.com>
2021-11-06 03:47:23 +0900

    環境構築

---
+++ gatsby-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Portfolio",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    `gatsby-plugin-typegen`,
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+// styles
+const pageStyles = {
+  color: "#232129",
+  padding: 96,
+  fontFamily: "-apple-system, Roboto, sans-serif, serif",
+};
+
+// markup
+const IndexPage = () => {
+  return <main style={pageStyles}></main>;
+};
+
+export default IndexPage;
