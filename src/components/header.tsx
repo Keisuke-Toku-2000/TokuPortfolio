4f0ee145828a4af37c2d7be60540c55d74d291f2
Keisuke-Toku-2000 <keisuke20000923@gmail.com>
2021-11-07 02:22:29 +0900

    reset for .gitignore

---
+++ src/components/header.tsx
@@ -0,0 +1,52 @@
+import React, { VFC } from "react";
+import { AppBar, Box, BoxProps, Toolbar } from "@material-ui/core";
+import { Link } from "gatsby";
+
+//style
+const headerLine: BoxProps = {
+  display: "inline",
+  marginX: 3,
+};
+
+const activeText: object = {
+  color: "white",
+  opacity: 1,
+  textDecoration: "none",
+  fontWeight: "bold",
+};
+
+const nonActiveText: object = {
+  color: "white",
+  opacity: 0.8,
+  textDecoration: "none",
+  fontWeight: "nomal",
+};
+
+const menuItem = ["TOP", "PROFILE", "BLOG"];
+
+export const Header: VFC = () => {
+  return (
+    <AppBar color="primary">
+      <Toolbar>
+        <Box marginLeft="auto">
+          {menuItem.map((item, i) => {
+            let page_link = "";
+            if (item === "TOP") {
+              page_link = "/";
+            } else page_link = "/" + item.toLowerCase() + "/";
+            return (
+              <Link
+                activeStyle={activeText}
+                style={nonActiveText}
+                to={page_link}
+                key={i}
+              >
+                <Box sx={headerLine}>{item}</Box>
+              </Link>
+            );
+          })}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
---
+++ src/pages/blog.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { Layout } from "../components/Layout/Layout";
+
+const Blog: React.VFC = () => {
+  return (
+    <Layout>
+      <div>Blog</div>
+    </Layout>
+  );
+};
+
+export default Blog;
